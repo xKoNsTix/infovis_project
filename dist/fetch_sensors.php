63dd2629e67e86c5ecbd68ccf29269b3109637ce
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2024-03-17 16:08:36 +0100

    added temp and lx sensor

--- dist/fetch_sensors.php
+++ dist/fetch_sensors.php
@@ -1,12 +1,11 @@
-
 <?php
 require_once '../config.php';
 header('Content-Type: application/json');
 $sensor_urls = [
     'energy_total' => "https://33d73ipserxnyyj9weae3lcmpoxx8omm.ui.nabu.casa/api/states/sensor.energy_total",
     'energy_current' => "https://33d73ipserxnyyj9weae3lcmpoxx8omm.ui.nabu.casa/api/states/sensor.energy_current",
-
-
+    'temperature_15' => "https://33d73ipserxnyyj9weae3lcmpoxx8omm.ui.nabu.casa/api/states/sensor.temperature_15",
+    'light_2' => "https://33d73ipserxnyyj9weae3lcmpoxx8omm.ui.nabu.casa/api/states/sensor.light_2",
     // Add more sensors here
 ];
 ini_set('display_errors', 1);
@@ -37,29 +36,21 @@ function fetchSensorData($sensorUrl)
 
 // Fetch and store sensor data individually
 $energy_total_data = fetchSensorData($sensor_urls['energy_total']);
-if ($energy_total_data) {
-    // Reformat the 'state' value for energy_total_data
-    $energy_total_data['state'] = str_replace('.', ',', $energy_total_data['state']);
-}
-
 $energy_current_data = fetchSensorData($sensor_urls['energy_current']);
-if ($energy_current_data) {
-    // Reformat the 'state' value for energy_current_data
-    $energy_current_data['state'] = str_replace('.', ',', $energy_current_data['state']);
-}
+$temperature_15_data = fetchSensorData($sensor_urls['temperature_15']);
+$light_2_data = fetchSensorData($sensor_urls['light_2']);
 
-
-// Repeat for other sensors
-
-
-
+// Reformatting the 'state' values for all sensors
+$energy_total_data['state'] = $energy_total_data ? str_replace('.', ',', $energy_total_data['state']) : 'N/A';
+$energy_current_data['state'] = $energy_current_data ? str_replace('.', ',', $energy_current_data['state']) : 'N/A';
+$temperature_15_data['state'] = $temperature_15_data ? str_replace('.', ',', $temperature_15_data['state']) : 'N/A';
+$light_2_data['state'] = $light_2_data ? str_replace('.', ',', $light_2_data['state']) : 'N/A';
 
 // Output the sensor data as JSON
-header('Content-Type: application/json');
 echo json_encode([
-    'energy_total' => $energy_total_data['state'] ?? 'N/A',
-    'energy_current' => $energy_current_data['state'] ?? 'N/A',
+    'energy_total' => $energy_total_data['state'],
+    'energy_current' => $energy_current_data['state'],
+    'temperature_15' => $temperature_15_data['state'],
+    'light_2' => $light_2_data['state'],
     // Add more sensor outputs here if needed
 ]);
-
-
